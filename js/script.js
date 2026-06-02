1b5253e70bf3ed015050ee705c5098f1c89800bb
vdovuhin <vdovuhinr@gmail.com>
2026-06-02 21:31:21 +0300

    init: start cssMemeSlider-task (Tue, Jun 02, 2026 09:31:21 PM)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
 
-let documentActions = (e) => {
-	const targetElement = e.target
-	if (targetElement.closest('.icon-menu')) {
-		document.documentElement.classList.toggle('open-menu')
-	}
-}
-
-document.addEventListener('click', documentActions)
